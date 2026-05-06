d5d8166ce4c94b5913e952d374ea954ad22774d7
Harshit Jaiswar <artharshitj@gmail.com>
2026-05-07 00:09:16 +0530

    added some winning patterns and eventListener in app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+let boxes = document.querySelectorAll('.box');
+let resetBtn = document.querySelector('.reset-btn');
+
+let turnO = true; //playerX , playerO
+
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        if (turnO) {
+            box.textContent = 'O';
+            turnO = false;
+        } else {
+            box.textContent = 'X';
+            turnO = true;
+        }
+    });
+});
